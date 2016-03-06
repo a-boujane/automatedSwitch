1315d4c99d37cb45282018df70c0aa5e71c3fa6f
abe.pi <abe.boujane@gmail.com>
2016-03-06 23:06:04 +0000

    added simple debug logging, Also, Added automated crontab event on boot using the automatedSwitch.sh script

---
+++ AutomatedSwitch.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#AutomatedSwitch.sh
+#Navigate to my GIT directory and launch the python script and then go back to  root
+
+cd /
+cd home/pi/AutomaticSwitch
+sudo python pyth.py
+cd /
--- pyth.py
+++ pyth.py
@@ -2,6 +2,10 @@
 
 import RPi.GPIO as GPIO
 import time
+#import datetime
+import logging
+
+#Selecting the GPIO for the sensor and the relay
 sensor = 4
 relay = 17
 
@@ -10,13 +14,19 @@ GPIO.setup(sensor,GPIO.IN,GPIO.PUD_DOWN)
 n=25
 previous_state = False
 current_state = False
+
+#initializing the LOG file name
+LOG_FILENAME='/home/pi/logs/AutomatedSwitch.log'
+
+logging.basicConfig(filename=LOG_FILENAME,level=logging.DEBUG)
+
 try:
    while True:
       time.sleep(0.1)
       previous_state=current_state
       n=n+1
       if n==20:
-         print "LOW"
+         logging.debug ('LOW - n==20, Cleaning up the relay')
          GPIO.cleanup(relay)
       if n==100:
          n=30
@@ -26,8 +36,8 @@ try:
          print (new_state)
          while current_state:
             GPIO.setup(relay,GPIO.OUT)
-            print ("let's leave it on for 120 seconds")
-            time.sleep(120)
+            logging.debug ("let's leave it on for 15 minutes")
+            time.sleep(900)
             current_state=GPIO.input(sensor)
             n=0
 except KeyboardInterrupt:
