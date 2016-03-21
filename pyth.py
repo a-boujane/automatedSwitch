320453c0b084954b2450016e836e52826d433813
abe.pi <abe.boujane@gmail.com>
2016-03-20 22:46:59 -0400

    a somewhat working version

--- pyth.py
+++ pyth.py
@@ -15,58 +15,63 @@ relay= 17
 
 
 def initialSetup ():
-	GPIO.cleanup
-	GPIO.setmode(GPIO.BCM)
-	GPIO.setup(sensor,GPIO.IN,GPIO.PUD_DOWN)
-	
+        GPIO.cleanup
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(sensor,GPIO.IN,GPIO.PUD_DOWN)
+        
 
 def ifMotionThenResetTimer(Q):
-	tt=int(time.time())
-	while True:		
-		motion = GPIO.input(sensor)
-		if motion:
-			tt=int(time.time())
-			print "motion detected! reset t0 to ", tt
-		Q.put(tt)
+        tt=int(time.time())
+        try:
+                while True:             
+                        motion = GPIO.input(sensor)
+                        if motion:
+                                tt=int(time.time())
+                                print "motion detected! reset t0 to ", tt
+                                Q.put(tt)
+        except KeyboardInterrupt:
+                GPIO.cleanup()
+                
 
 def ifTimerThenTurnOnOrOff(Q):
-	alreadyOn=False
-	tt=int(time.time())
-	while True:
-		tt=Q.get(tt)
-		t=int(time.time())
-		delta = t-tt
-		print delta
-		if t-tt<10 and not alreadyOn:
-			print "if t-t0<10 and not alreadyOn:"
-			turnOn()
-			alreadyOn=True
-		elif t-tt>=10 and not alreadyOn:
-			print "elif t-t0>=10 and not alreadyOn:"
-			turnOff()
-			alreadyOff=False
+        alreadyOn=False
+        
+        tt=int(time.time())
+        try:
+                while True:
+                        if not Q.empty():
+                                tt=Q.get()
+                        t=int(time.time())
+                        delta = t-tt
+                        print delta
+                        if delta<10 and not alreadyOn:
+                                print "if t-t0<10 and not alreadyOn:"
+                                turnOn()
+                                alreadyOn=True
+                        elif delta>=10 and alreadyOn:
+                                print "elif t-t0>=10 and not alreadyOn:"
+                                turnOff()
+                                alreadyOn=False
+        except KeyboardInterrupt:
+                GPIO.cleanup()
 
 def turnOn():
-	print "entered turnOn()"
-	print "going HIGH now"
-	GPIO.setup(relay,GPIO.OUT)
+        print "entered turnOn()"
+        print "going HIGH now"
+        GPIO.setup(relay,GPIO.OUT)
 
 def turnOff():
-	print "Entered turnOff()"
-	print "Going LOW now"
-	GPIO.cleanup(relay)
+        print "Entered turnOff()"
+        print "Going LOW now"
+        GPIO.cleanup(relay)
 
 initialSetup()
 Q=Queue.Queue()
 
-try:
-	Thread1=threading.Thread(target=ifMotionThenResetTimer, args=(Q,))
-	Thread2=threading.Thread(target=ifTimerThenTurnOnOrOff, args=(Q,))
-	print "Starting Thread1, then Thread2"
-	Thread1.start()
-	Thread2.start()
-except KeyboardInterrupt:
-	Thread1.stop()
-	Thread2.stop()
-	GPIO.cleanup()
-	
+Thread1=threading.Thread(target=ifMotionThenResetTimer, args=(Q,))
+Thread2=threading.Thread(target=ifTimerThenTurnOnOrOff, args=(Q,))
+print "Starting Thread1, then Thread2"
+Thread1.start()
+Thread2.start()
+
+        
